7ab3787355cde065fd12b19c43b7628f30db1b5d
Volodymyr <brintsov@gmail.com>
2024-03-16 02:42:21 -0800

    v-text. v-html

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
     <h1>Hello Vue!</h1>
-    <h2>Typy proste - ref</h2>
-    <p>Kurs: {{ courseName }}, uczestnicy: {{ courseParticipants }}</p>
-    <h2>Obiekty - reactive</h2>
-    <p>Kurs: {{  course.name }}, uczestnicy: {{ course.participants }}</p>
+    <p v-html="'Kurs: <b>' + course.name +'</b>, uczestnicy: ' + course.participants"></p>
+    <p v-text="'Kurs: <b>' + course.name +'</b>, uczestnicy: ' + course.participants"></p>
 </template>
 
 <script setup>
-    import { ref, reactive } from 'vue'
-    const courseName = ref('Bogate aplikacje internetowe')
-    const courseParticipants = ref(20)
-    const course = reactive({
-         name: 'Programowanie kreatywne',
-         participants: 10
-    })
-    courseParticipants.value++
-    course.participants++
-</script> vue
+    import { reactive } from 'vue'
+    const course = reactive({ name: 'meme kreatywne', participants: 10 })
+</script>
